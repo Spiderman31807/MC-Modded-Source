a70f9a7566816a01b95b5fda44bc9a6d6e7335a5
Spiderman31807 <Spiderman31807@gmail.com>
2024-09-30 08:57:39 +0100

    Add files via upload

---
+++ Adaptive Difficultly/1.20.1/Forge/V1.0.0/src/main/java/spiderman/adaptive_difficulty/procedures/SetTargetProcedure.java
@@ -0,0 +1,36 @@
+package spiderman.adaptive_difficulty.procedures;
+
+import spiderman.adaptive_difficulty.Adaptive;
+
+import net.minecraftforge.fml.common.Mod;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
+import net.minecraftforge.eventbus.api.Event;
+import net.minecraftforge.event.entity.living.LivingChangeTargetEvent;
+
+import net.minecraft.world.Difficulty;
+import net.minecraft.world.entity.player.Player;
+import net.minecraft.resources.ResourceLocation;
+import net.minecraft.core.registries.Registries;
+import net.minecraft.tags.TagKey;
+
+@Mod.EventBusSubscriber
+public class SetTargetProcedure {
+	@SubscribeEvent
+	public static void onEntitySetsAttackTarget(LivingChangeTargetEvent event) {
+		if(event.getNewTarget() instanceof Player player) {
+			Difficulty adaptive = Adaptive.getDifficulty(player);
+			Difficulty difficulty = player.level().getDifficulty();
+			int difficultyId = Adaptive.getDifficultyId(player);
+
+			if(Adaptive.isSameDifficulty(difficultyId, player.level()))
+				return;
+					
+			if(difficultyId == 0 && !event.getEntity().getType().is(TagKey.create(Registries.ENTITY_TYPE, new ResourceLocation("adaptive:peaceful_targeting")))) {
+				if (event != null && event.isCancelable())
+					event.setCanceled(true);
+				if (event != null && event.hasResult())
+					event.setResult(Event.Result.DENY);
+			}
+		}
+	}
+}
